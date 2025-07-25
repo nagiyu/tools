29ca9d4909fbd8bab142b44f2eecb03d7e327fec
nagiyu <153594832+nagiyu@users.noreply.github.com>
2025-07-25 13:36:53 +0900

    Update BasicDatePicker.tsx

--- client/common/components/inputs/Dates/BasicDatePicker.tsx
+++ client/common/components/inputs/Dates/BasicDatePicker.tsx
@@ -1,23 +1,38 @@
-import * as React from 'react';
+import React from 'react';
+
+import dayjs, { Dayjs } from 'dayjs';
+
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import TextField from '@mui/material/TextField';
 
-interface BasicDatePickerProps {
-  value: Date | null;
-  onChange: (date: Date | null) => void;
-  minDate?: Date;
-  maxDate?: Date;
-}
-
-export const BasicDatePicker: React.FC<BasicDatePickerProps> = ({ value, onChange, minDate, maxDate }) => {
-  return (
-    <DatePicker
-      label="日付を選択"
-      value={value}
-      onChange={onChange}
-      minDate={minDate}
-      maxDate={maxDate}
-      renderInput={(params) => <TextField {...params} />}
-    />
-  );
+type BasicDatePickerProps = {
+    label?: string;
+    value: Date | null;
+    onChange: (date: Date | null) => void;
 };
+
+export default function BasicDatePicker({
+    label = '',
+    value,
+    onChange
+}: BasicDatePickerProps) {
+    // Date → dayjs 変換
+    const dayjsValue = value ? dayjs(value) : null;
+
+    // onChange: dayjs → Date 変換
+    const handleChange = (newValue: Dayjs | null) => {
+        onChange(newValue ? newValue.toDate() : null);
+    };
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+                label={label}
+                format='YYYY/MM/DD'
+                value={dayjsValue}
+                onChange={handleChange}
+            />
+        </LocalizationProvider>
+    );
+}
