f618ed01515372c70bddbf2d6ae3567d3c35347c
nagiyu <otonagiyu@gmail.com>
2025-07-25 12:56:54 +0000

    fix(page): add missing newline for better code readability

--- client/finance/app/page.tsx
+++ client/finance/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+
 import CandleStick, { CandleStickData } from '@client-common/components/echarts/CandleStick';
 
 export default function Home() {
