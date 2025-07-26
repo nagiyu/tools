33c79086d3f3e83766364718e36b0dcf997bf0f3
nagiyu <153594832+nagiyu@users.noreply.github.com>
2025-07-26 23:41:58 +0900

    Update layout.tsx

--- client/nextjs-sample/app/layout.tsx
+++ client/nextjs-sample/app/layout.tsx
@@ -35,8 +35,7 @@ async function getMenuItems(): Promise<MenuItemData[]> {
     { title: 'Sample Tab', url: '/sample-tab' },
     { title: 'Sample Text Field', url: '/sample-text-field' },
     { title: 'Sample Radio Group', url: '/sample-radio-group' },
-    { title: 'Sample Dialogs', url: '/sample/dialogs' },
-
+    { title: 'Sample Dialogs', url: '/sample/dialogs' }
   ];
 
   if (session) {
