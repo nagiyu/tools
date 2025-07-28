ea06a2b99d9b9c665d1c2db71517bb80bc5351ea
nagiyu <153594832+nagiyu@users.noreply.github.com>
2025-07-28 22:13:34 +0900

    Update BasicTable.tsx

--- client/common/components/data/table/BasicTable.tsx
+++ client/common/components/data/table/BasicTable.tsx
@@ -22,6 +22,7 @@ interface BasicTableProps<T> {
   columns: Column<T>[];
   data: T[];
 }
+
 interface BasicTableProps<T> {
   columns: Column<T>[];
   data: T[];
@@ -108,68 +109,6 @@ function BasicTable<T>({
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
       />
-    </Paper>
-  );
-}
-
-export default BasicTable;
-
-
-function BasicTable<T>({ columns, data }: BasicTableProps<T>) {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
-  return (
-    <Paper>
-      <TableContainer>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={String(column.id)}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, rowIndex) => (
-              <TableRow hover role="checkbox" tabIndex={-1} key={rowIndex}>
-                {columns.map((column) => {
-                  const value = row[column.id];
-                  return (
-                    <TableCell key={String(column.id)} align={column.align}>
-                      {column.format ? column.format(value) : (value as React.ReactNode)}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableBody>
-          <TablePagination
-            rowsPerPageOptions={[10, 25, 100]}
-            component="div"
-            count={data.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Table>
-      </TableContainer>
     </Paper>
   );
 }
